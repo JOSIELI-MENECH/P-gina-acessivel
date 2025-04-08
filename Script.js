33265a55c57d374cea54c133d118242b96f4fe2a
JOSIELI-MENECH <josieli.menech@escola.pr.gov.br>
2025-04-08 11:19:29 -0300

    dia 08 de abril

--- Script.js
+++ Script.js
@@ -1,29 +1,31 @@
 
 document.addEventListener('DOMContentLoaded',function(){
 
-    const botaoAcessibilidade = document.getElementById('botao-acessibilidade');
-    const opcoesAcessibilidade= document.getElementById('opcoes-acessbilidade');
-    botaoAcessibilidade.addEventListener('click', function(
-
-    ) {
-    botaoAcessibilidade.classList.toggle('rotacao-botao');
-    opcoesAcessibilidade.classList.toggle('apresenta-lista')};
-    )}
-    );
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+    const opcoesDAcessibilidade= document.getElementById('opcoes-acessbilidade');
+    
+    botaoDeAcessibilidade.addEventListener('click', function(){
+    botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+    opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+    })
 
 
     const aumentarFonteBotao = document.getElementById('aumentar-fonte');
     const diminuirFonteBotao = document.getElementById('diminuir-fonte');
     
-    let tamanhoAtualFont= 1;
+    let tamanhoAtualFonte= 1;
     
     aumentarFonteBotao.addEventListener('click', function(){
-    tamanhoAtualFont += 0.1;
+    tamanhoAtualFonte += 0.1;
     document.body.style.fontSize = `${tamanhoAtualFonte}rem`
     })
     diminuirFonteBotao.addEventListener('click', function(){
-        tamanhoAtualFont += 0.1;
+        tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
+})
+
+alternaContraste.addEventListener('click', function () {
+    document.body.classList.toggle('alto-contraste')
 })
 })
